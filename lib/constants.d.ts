2f76475fe4b680db876e0151bc7554e1a1a7b066
James Ives <iam@jamesiv.es>
2020-12-11 12:44:08 +0000

    Deploy production js code for 26b51152dbe5adc3ad8bceadbc11e2edee7bc3ff

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -1,18 +1,16 @@
 export interface ActionInterface {
     /** Deployment access token. */
     accessToken?: string | null;
-    /** The base branch that the deploy should be made from. */
-    baseBranch?: string;
     /** The branch that the action should deploy to. */
     branch: string;
+    /** git push with --dry-run */
+    dryRun?: boolean | null;
     /** If your project generates hashed files on build you can use this option to automatically delete them from the deployment branch with each deploy. This option can be toggled on by setting it to true. */
     clean?: boolean | null;
     /** If you need to use CLEAN but you'd like to preserve certain files or folders you can use this option. */
     cleanExclude?: string | string[];
     /** If you need to customize the commit message for an integration you can do so. */
     commitMessage?: string;
-    /** The default branch of the deployment. Similar to baseBranch if you're using this action as a module. */
-    defaultBranch?: string;
     /** The git config email. */
     email?: string;
     /** The folder to deploy. */
@@ -23,12 +21,8 @@ export interface ActionInterface {
     gitHubToken?: string | null;
     /** Determines if the action is running in test mode or not. */
     isTest?: boolean | null;
-    /** Removes files from LFS if toggled to allow normal deployment. */
-    lfs?: boolean | null;
     /** The git config name. */
     name?: string;
-    /** Determines if the workspace should be stashed/restored prior to comitting. */
-    preserve?: boolean | null;
     /** The repository path, for example JamesIves/github-pages-deploy-action. */
     repositoryName?: string;
     /** The fully qualified repositpory path, this gets auto generated if repositoryName is provided. */
